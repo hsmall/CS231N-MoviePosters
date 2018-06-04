844312422d0fa3c305280841833489230d6f32d8
Kimberly Wijaya <kcwijaya@stanford.edu>
2018-06-04 00:25:38 -0700

    Edited GAN

--- GenreDataset.py
+++ GenreDataset.py
@@ -8,20 +8,25 @@ class GenreDataset():
 		dirname = "posters/"+genre
 		image_list = []
 		for filename in glob.glob('posters/' + genre + '/*.jpg'):
-			print(filename)
 			im=Image.open(filename)
+			img=im.convert("RGB")
+			im=img.resize((64, 64))
 			image_list.append(np.array(im))
 
 		self.X = np.array(image_list)
 		self.X = self.X.astype(np.float32)/255
-		self.X = np.reshape(self.X, (self.X.shape[0], -1))
+		self.X = self.X[:, :, :, :3]
 		self.batch_size = batch_size
+		print(self.X.shape) 
+
+	def num_batches(self): 
+		return int(np.ceil(self.X.shape[0])/self.batch_size)
 
 	def __iter__(self): 
 		N, B = self.X.shape[0], self.batch_size 
 		idxs = np.arange(N)
 
-		return iter((self.X[i:i+B], self.y[i:i+B]) for i in range(0, N))
+		return iter((self.X[i:i+B]) for i in range(0, N, B))
 
 if __name__ == '__main__':
 	main()
